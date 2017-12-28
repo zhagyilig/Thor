653ba8406f7c0831e002ae2eae00562f8fefab7f
xtrdb.net <813415154@qq.com>
2017-12-28 18:07:28 +0800

    :santa:

---
+++ scripts/test.py
@@ -0,0 +1,14 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+
+from fabric.api import *
+from fabric.colors import *
+from fabric.context_managers import *
+
+def test_sc():
+    print("ok")
+
+
+
+
+
